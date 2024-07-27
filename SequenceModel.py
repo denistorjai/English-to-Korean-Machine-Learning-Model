2c497ca47898ec7941268ab7e9803ac9cfb03681
denistorjai <denistorjai@outlook.com>
2024-07-27 02:14:06 -0600

    Update SequenceModel.py

--- SequenceModel.py
+++ SequenceModel.py
@@ -9,5 +9,38 @@ import DatasetLoader
 class Encoder(nn.Module):
     def __init__(self, ModelInput, EmbeddedSize, HiddenSize, NumberofLayers, DropoutRate):
         super().__init__()
+
+        # Set Class variables
         self.Embedding = nn.Embedding(ModelInput, EmbeddedSize)
-        
+        self.rnn = nn.LSTM(EmbeddedSize, HiddenSize, NumberofLayers, dropout=DropoutRate)
+        self.dropout = nn.Dropout(DropoutRate)
+
+    # encode forward function
+    def forward(self, src):
+        embedded = self.dropout(self.Embedding(src))
+        outputs, (hidden, cell) = self.rnn(embedded)
+
+        # return encode
+        return hidden, cell
+    
+# Decode 
+class Decoder(nn.Module):
+    def __init__(self, ModelOutput, EmbeddedSize, HiddenSize, NumberofLayers, DropoutRate):
+        super().__init__()
+
+        # Class variables
+        self.Embedding = nn.Embedding(ModelOutput, EmbeddedSize)
+        self.rnn = nn.LSTM(EmbeddedSize, HiddenSize, NumberofLayers, dropout=DropoutRate)
+        self.fc_out = nn.Linear(HiddenSize, ModelOutput)
+        self.dropout = nn.Dropout(DropoutRate)
+
+    # decode forward function
+    def forward(self, input, hidden, cell):
+        input = input.unsqueeze(0)
+        embedded = self.dropout(self.Embedding(input))
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
+        prediction = self.fc_out(output.squeeze(0))
+
+        # return decode
+        return prediction, hidden, cell
+    
